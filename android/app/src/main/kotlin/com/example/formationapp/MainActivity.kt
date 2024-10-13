163204c051966d8b0ec4cb23c94f0299395dd7de
Khamine98 <kh.amine98@gmail.com>
2024-10-13 17:45:35 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/formationapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.formationapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/page1.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+
+/*import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("app title")),
+        body: Container(
+          decoration: BoxDecoration(color: Colors.amber,
+          borderRadius:BorderRadius.all(Radius.circular(15)),
+          border: Border.all(color: Colors.black, width: 2),
+          boxShadow: [
+            BoxShadow(color: Colors.yellow, offset:  Offset(1, 1), spreadRadius: 3, blurRadius: 20)]),
+          width: 400,
+          height: 400,
+          alignment: Alignment.center,
+          margin: EdgeInsets.all(10),
+          padding: EdgeInsets.all(10),
+         child: const Text("Amine KHALED", style: TextStyle(
+          color: Colors.black,
+          fontSize: 40.2,
+          fontWeight: FontWeight.w900
+        ))),
+      )
+    );
+  }
+}
+*/
